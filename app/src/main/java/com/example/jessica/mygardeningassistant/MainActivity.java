39bde6d83f6dfaa2a14add080efc0687a919fcf2
Jessica <jeh15g@acu.edu>
2018-11-26 16:17:41 -0600

    Fixed transparency and started My Garden page

--- app/src/main/java/com/example/jessica/mygardeningassistant/MainActivity.java
+++ app/src/main/java/com/example/jessica/mygardeningassistant/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jessica.mygardeningassistant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goMyGarden (View view) {
+        Intent intent = new Intent(this, MyGarden.class);
+        startActivity(intent);
     }
 }
